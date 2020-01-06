8a52aa04678fbabdffd2df3d801e4f8e2480690c
MFarrell242 <farrell.michael.w@gmail.com>
2020-01-06 14:02:32 -0600

    don't know if it'll work since the interface is weird, but it's something.

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/api/notes", function (req, res){
-    fs.readFile("./db.json", 'utf8', (err, data) =>{
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db.json", 'utf8', (err, data) => {
         if (err) throw err;
         notes = JSON.parse(data);
         return res.json(notes);
@@ -24,12 +24,34 @@ app.get("/api/notes", function (req, res){
 });
 app.post("/api/notes", (req, res) => {
     var newNote = req.body;
+    var id = 0;
+    fs.readFile("./db,json", 'utf8', (err, data) => {
+        if (err) throw err;
+        id = data.length + 1;
+    });
     newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+    newNote.ID = id;
     console.log(newNote);
     fs.appendFile("db.json", newNote);
     res.json(newNote);
+});
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("db.json", 'utf8', (err, data) => {
+        var remNotes = data;
+        remNotes.forEach(item => {
+            if (item.ID = req.params.id) {
+                let where = remNotes.indexOf(item);
+                remNotes.splice(where, 1);
+                for (var q = where; q < remNotes.length; q++) {
+                    remNotes[q].ID -= 1;
+                }
+                return ("done, yo");
+            }
+        })
+    })
+    
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`ready to take notes on port ${PORT}`);
 });
